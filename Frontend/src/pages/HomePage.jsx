1b22b2aa35d4b0de537ce578fd7e92e174e95577
vishal-rajput1 <rahputvishal1322@gmail.com>
2026-08-01 17:44:33 +0530

    Fix component import casing

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useChatStore } from '../store/useChatStore'
-import Sidebar from '../components/sidebar';
+import Sidebar from '../components/Sidebar';
 import NoChatSelected from '../components/NoChatSelected';
 import ChatContainer from '../components/ChatContainer';
 
