5488a3b088491c7530af58600e56882ef6fa9b16
vishal-rajput1 <rahputvishal1322@gmail.com>
2026-08-01 17:47:56 +0530

    Fix component import casing

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -31,9 +31,9 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
 }
 
 server.listen(PORT, () => {
